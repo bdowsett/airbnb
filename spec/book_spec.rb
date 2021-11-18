30bc5cebe0c80cb23da4febe3b7df3bfc64a415d
Ben Dowsett <bendowsett@gmail.com>
2021-11-18 15:36:40 +0000

    failing test with create

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,7 +2,7 @@ describe Booking do
     it "creates a new booking" do
         Account.create("Ben", "email", "100000", "password" )
         account_id = Account.find_account_id("Ben")
-        Booking.create("House", "2021-12-12", account_id)
+        Booking.create("House", "2021-12-12", 1)
         expect(Booking.all.first.name).to eq "House"
         expect(Booking.all.first.date).to eq "2021-12-12"
         expect(Booking.all.first.account_id).to eq account_id
